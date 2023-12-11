e1e014b019c0e5573770c6ba47178af4582178e0
Oxity <oxity.productions@gmail.com>
2023-12-11 11:12:34 +0100

    Update guess_the_number.py
    Works with user-defined values

--- guess_the_number.py
+++ guess_the_number.py
@@ -5,8 +5,17 @@ class GuessTheNumber:
     def __int__(self):
         minimum = 1
         maximum = 100
+        
+    def define_values(self):
+        try:
+            self.minimum = int(input("Enter a minimum value: "))
+            self.maximum = int(input("Enter a maximum value: "))
+            self.number = random.randint(self.minimum, self.maximum)
+        except ValueError:
+            print("Invalid input!")
+            self.define_values()
 
-    def guess_the_number(self, minimum, maximum):
+    def guess_the_number(self):
         guess = input("Guess the number: ")
         try:
             if int(guess) < self.number:
@@ -23,4 +32,5 @@ class GuessTheNumber:
 
 
 GTN = GuessTheNumber()
+GTN.define_values()
 GTN.guess_the_number()
